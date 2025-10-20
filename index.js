309ccb42d04ec1f49a18d457d670be5039afef0e
montitripathi12345@gmail.com <vivektripathi8662@gmail.com>
2025-10-20 19:50:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js App running via CI/CD!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
